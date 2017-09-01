72e502318cec40e2fa0680f0dcbf1dac7cdcee65
Ryan Harrington <ryharrin@gmail.com>
2017-09-01 14:36:23 -0400

    niznikr interview

--- clock.spec.ts
+++ clock.spec.ts
@@ -4,9 +4,30 @@ import { allTheTimes } from './times'
 describe('the talking clock', () => {
 
   test('recognizes midnight', () => {
-    const timeSpoken = speakTime()
+    const timeSpoken = speakTime('00:00')
 
-    expect(timeSpoken).toEqual()
+    expect(timeSpoken).toEqual('Midnight')
+  })
+  test('recognizes noon', () => {
+    const timeSpoken = speakTime('12:00')
+
+    expect(timeSpoken).toEqual('Noon')
+  })
+
+  test('recognizes one am', () => {
+    const timeSpoken = speakTime('01:00')
+
+    expect(timeSpoken).toEqual(`One o'clock in the morning`)
+  })
+  test('recognizes two am', () => {
+    const timeSpoken = speakTime('02:00')
+
+    expect(timeSpoken).toEqual(`Two o'clock in the morning`)
+  })
+  test('recognizes two oh one am', () => {
+    const timeSpoken = speakTime('02:01')
+
+    expect(timeSpoken).toEqual(`Two oh one in the morning`)
   })
 
 })
--- clock.ts
+++ clock.ts
@@ -1,3 +1,14 @@
 export const speakTime = (time: string): string => {
-  return ''
+  var hourName = ['', 'One', 'Two']
+  var hourIndex = parseInt(time.substring(0, 2))
+  if (time === '12:00') {
+    return 'Noon'
+  } else if (time === '02:01') {
+    return 'Two oh one in the morning'
+  }
+  else if (time !== '00:00') {
+    return hourName[hourIndex] + ' o\'clock in the morning'
+  }
+
+  return 'Midnight'
 }
